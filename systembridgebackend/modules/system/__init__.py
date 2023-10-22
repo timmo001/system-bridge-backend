f38322fdb71e5933f1b8fcb770fff340ea343801
Aidan Timson <aidan@timmo.dev>
2023-10-22 23:28:47 +0100

    Allow import to fail

--- systembridgebackend/modules/system/__init__.py
+++ systembridgebackend/modules/system/__init__.py
@@ -5,16 +5,11 @@ import os
 import platform
 import re
 import socket
+import sys
 import uuid
-from typing import Optional
+from typing import Any, Optional
 
-from aiogithubapi import (
-    GitHubAPI,
-    GitHubConnectionException,
-    GitHubException,
-    GitHubRatelimitException,
-    GitHubReleaseModel,
-)
+import aiohttp
 from pkg_resources import parse_version
 from plyer import uniqueid
 from psutil import boot_time, users
@@ -24,6 +19,10 @@ from systembridgeshared.database import Database
 from systembridgeshared.models.database_data import System as DatabaseModel
 
 from ..._version import __version__
+
+SERVER_SOFTWARE = "{0}/{1} aiohttp/{2} Python/{3[0]}.{3[1]}".format(
+    "SystemBridge", __version__, aiohttp.__version__, sys.version_info
+)
 
 
 class System(Base):
@@ -76,22 +75,37 @@ class System(Base):
         """Get version"""
         return __version__.public()
 
-    async def version_latest(self) -> Optional[GitHubReleaseModel]:
+    async def version_latest(self) -> Optional[Any]:
         """Get latest version from GitHub"""
         self._logger.info("Get latest version from GitHub")
         try:
-            async with GitHubAPI() as github:
-                releases = await github.repos.releases.list("timmo001/system-bridge")
-            return releases.data[0] if releases.data else None
-        except (
-            GitHubConnectionException,
-            GitHubRatelimitException,
-        ) as error:
-            self._logger.error("Error getting data from GitHub: %s", error)
-        except GitHubException as error:
-            self._logger.exception(
-                "Unexpected error getting data from GitHub: %s", error
+            from aiogithubapi import (  # pylint: disable=import-outside-toplevel
+                GitHubAPI,
+                GitHubConnectionException,
+                GitHubException,
+                GitHubRatelimitException,
             )
+
+            try:
+                async with GitHubAPI(
+                    token="",
+                    **{"client_name": SERVER_SOFTWARE},  # type: ignore
+                ) as github:
+                    releases = await github.repos.releases.list(
+                        "timmo001/system-bridge"
+                    )
+                return releases.data[0] if releases.data else None
+            except (
+                GitHubConnectionException,
+                GitHubRatelimitException,
+            ) as error:
+                self._logger.error("Error getting data from GitHub: %s", error)
+            except GitHubException as error:
+                self._logger.exception(
+                    "Unexpected error getting data from GitHub: %s", error
+                )
+        except ImportError as error:
+            self._logger.error("Unable to import aiogithubapi: %s", error)
         return None
 
     def version_newer_available(
