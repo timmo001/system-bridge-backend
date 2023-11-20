95df9603f01714f3f4a18dbc2cfcc0943af0e8d0
Aidan Timson <aidan@timmo.dev>
2023-11-20 22:33:20 +0000

    Staggered updates on multiple threads

--- systembridgebackend/modules/__init__.py
+++ systembridgebackend/modules/__init__.py
@@ -1,6 +1,7 @@
 """Modules"""
 import asyncio
 from collections.abc import Awaitable, Callable
+from threading import Thread
 from typing import Any
 
 from systembridgemodels.sensors import Sensors
@@ -32,6 +33,32 @@ MODULES = [
 ]
 
 
+class UpdateThread(Thread):
+    """Update thread"""
+
+    def __init__(
+        self,
+        class_obj: dict[str, Any],
+        updated_callback: Callable[[str, Any], Awaitable[None]],
+    ) -> None:
+        """Initialise"""
+        super().__init__()
+        self._class_obj = class_obj
+        self._updated_callback = updated_callback
+
+    async def _update(self) -> None:
+        """Update"""
+        data = await self._class_obj["cls"].update_all_data()
+        await self._updated_callback(
+            self._class_obj["name"],
+            data,
+        )
+
+    def run(self) -> None:
+        """Run"""
+        asyncio.run(self._update())
+
+
 class Update(Base):
     """Modules Update"""
 
@@ -43,7 +70,9 @@ class Update(Base):
         super().__init__()
         self._updated_callback = updated_callback
 
-        self._classes = [
+        self._classes: list[dict[str, Any]] = [
+            # System first
+            {"name": "system", "cls": SystemUpdate()},
             {"name": "battery", "cls": BatteryUpdate()},
             {"name": "cpu", "cls": CPUUpdate()},
             {"name": "disks", "cls": DisksUpdate()},
@@ -52,24 +81,7 @@ class Update(Base):
             {"name": "memory", "cls": MemoryUpdate()},
             {"name": "networks", "cls": NetworksUpdate()},
             {"name": "processes", "cls": ProcessesUpdate()},
-            {"name": "system", "cls": SystemUpdate()},
         ]
-
-    async def _update(
-        self,
-        sensors_data: Sensors,
-        class_obj: dict,
-    ) -> None:
-        """Update"""
-        # If the class has a sensors attribute, set it
-        if class_obj["cls"].__dict__.get("sensors", {}) != {}:
-            class_obj["cls"].sensors = sensors_data
-
-        data = await class_obj["cls"].update_all_data()
-        await self._updated_callback(
-            class_obj["name"],
-            data,
-        )
 
     async def update_data(self) -> None:
         """Update Data"""
@@ -79,8 +91,18 @@ class Update(Base):
         sensors_data = await sensors_update.update_all_data()
         await self._updated_callback("sensors", sensors_data)
 
-        # TODO: Update data in separate threads
-        tasks = [self._update(sensors_data, cls) for cls in self._classes]
-        await asyncio.gather(*tasks)
+        for class_obj in self._classes:
+            # If the class has a sensors attribute, set it
+            if class_obj["cls"].__dict__.get("sensors", {}) != {}:
+                class_obj["cls"].sensors = sensors_data
 
-        self._logger.info("Finished updating data")
+            thread = UpdateThread(
+                class_obj,
+                self._updated_callback,
+            )
+            thread.start()
+
+            # Stagger the updates to avoid overloading the system
+            await asyncio.sleep(1)
+
+        self._logger.info("Data updates requested")
