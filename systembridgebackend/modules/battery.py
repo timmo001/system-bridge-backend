4a16b31ce94da574d4837f830cc6229f18202737
Aidan Timson <aidan@timmo.dev>
2024-01-03 01:11:59 +0000

    Get time remaining from psutil sensors

--- systembridgebackend/modules/battery.py
+++ systembridgebackend/modules/battery.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 from typing import override
 
 from plyer import battery
+import psutil
 
 from systembridgemodels.modules.battery import Battery
 
@@ -13,10 +14,23 @@ from .base import ModuleUpdateBase
 class BatteryUpdate(ModuleUpdateBase):
     """Battery Update."""
 
+    def _get_sensors(self) -> psutil._common.sbattery | None:  # type: ignore
+        """Get battery sensors."""
+        if not hasattr(psutil, "sensors_battery"):
+            return None
+        return psutil.sensors_battery()  # type: ignore
+
     @override
     async def update_all_data(self) -> Battery:
         """Update all data."""
         self._logger.debug("Update all data")
+
+        sensors = self._get_sensors()
+        time_remaining = (
+            sensors.secsleft
+            if sensors is not None and hasattr(sensors, "secsleft")
+            else None
+        )
 
         status = battery.status
         if (status) is None or (
@@ -25,10 +39,10 @@ class BatteryUpdate(ModuleUpdateBase):
             return Battery(
                 is_charging=None,
                 percentage=None,
-                time_remaining=None,
+                time_remaining=time_remaining,
             )
         return Battery(
             is_charging=status["isCharging"],
             percentage=status["percentage"],
-            time_remaining=status["timeRemaining"],
+            time_remaining=time_remaining,
         )
