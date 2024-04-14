df6858a2487874e09adcaab61c447f680fd35e12
timmo001 <timmo001@users.noreply.github.com>
2024-04-14 12:23:15 +0000

    Bump systembridgebackend version to 4.2.5.dev0

--- systembridgebackend/_version.py
+++ systembridgebackend/_version.py
@@ -7,5 +7,5 @@ Provides systembridgebackend version information.
 
 from incremental import Version
 
-__version__ = Version("systembridgebackend", 4, 2, 4)
+__version__ = Version("systembridgebackend", 4, 2, 5, dev=0)
 __all__ = ["__version__"]
