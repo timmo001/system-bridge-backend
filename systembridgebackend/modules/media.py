a171a5a64c4c8b2a1c7ec36ad6603be8d711dde5
Aidan Timson <contact@timmo.xyz>
2024-08-13 11:41:26 +0100

    Handle media apis not avaliable

--- systembridgebackend/modules/media.py
+++ systembridgebackend/modules/media.py
@@ -87,91 +87,94 @@ class Media(Base):
     async def update_media_info(self) -> None:
         """Update media info from the current session."""
         if platform.system() != "Windows":
-            return None
+            return
 
-        if (
-            self.sessions is not None
-            and self.current_session_changed_handler_token is not None
-        ):
-            self.sessions.remove_current_session_changed(
-                self.current_session_changed_handler_token
-            )
-
-        self.sessions = (
-            await wmc.GlobalSystemMediaTransportControlsSessionManager.request_async()
-        )
-        self.current_session_changed_handler_token = (
-            self.sessions.add_current_session_changed(
-                self._current_session_changed_handler
-            )
-        )
-
-        if self.current_session is not None:
-            if self.properties_changed_handler_token is not None:
-                self.current_session.remove_media_properties_changed(
-                    self.properties_changed_handler_token
-                )
-            if self.playback_info_changed_handler_token is not None:
-                self.current_session.remove_playback_info_changed(
-                    self.playback_info_changed_handler_token
-                )
-
-        self.current_session = self.sessions.get_current_session()
-        if self.current_session:
-            self.properties_changed_handler_token = (
-                self.current_session.add_media_properties_changed(
-                    self._properties_changed_handler
-                )
-            )
-            self.playback_info_changed_handler_token = (
-                self.current_session.add_playback_info_changed(
-                    self._playback_info_changed_handler
-                )
-            )
-            media_info = MediaInfo()
-            if info := self.current_session.get_playback_info():
-                media_info.status = info.playback_status.name
-                media_info.playback_rate = info.playback_rate
-                media_info.shuffle = info.is_shuffle_active
-                if info.auto_repeat_mode:
-                    media_info.repeat = info.auto_repeat_mode.name
-                if info.playback_type:
-                    media_info.type = info.playback_type.name
-                if info.controls:
-                    media_info.is_fast_forward_enabled = (
-                        info.controls.is_fast_forward_enabled
-                    )
-                    media_info.is_next_enabled = info.controls.is_next_enabled
-                    media_info.is_pause_enabled = info.controls.is_pause_enabled
-                    media_info.is_play_enabled = info.controls.is_play_enabled
-                    media_info.is_previous_enabled = info.controls.is_previous_enabled
-                    media_info.is_rewind_enabled = info.controls.is_rewind_enabled
-                    media_info.is_stop_enabled = info.controls.is_stop_enabled
-
-            if timeline := self.current_session.get_timeline_properties():
-                media_info.duration = timeline.end_time.total_seconds()
-                media_info.position = timeline.position.total_seconds()
-
+        try:
             if (
-                properties
-                := await self.current_session.try_get_media_properties_async()
+                self.sessions is not None
+                and self.current_session_changed_handler_token is not None
             ):
-                media_info.title = properties.title
-                media_info.subtitle = properties.subtitle
-                media_info.artist = properties.artist
-                media_info.album_artist = properties.album_artist
-                media_info.album_title = properties.album_title
-                media_info.track_number = properties.track_number
+                self.sessions.remove_current_session_changed(
+                    self.current_session_changed_handler_token
+                )
 
-            media_info.updated_at = datetime.datetime.now().timestamp()
-
-            await self._update_data(media_info)
-
-            if media_info.status == "PLAYING":
-                self.update_media_info_interval(PLAYING_UPDATE_INTERVAL)
-            else:
-                self.update_media_info_interval(IDLE_UPDATE_INTERVAL)
-        else:
-            await self._update_data(
-                MediaInfo(updated_at=datetime.datetime.now().timestamp())
+            self.sessions = (
+                await wmc.GlobalSystemMediaTransportControlsSessionManager.request_async()
             )
+            self.current_session_changed_handler_token = (
+                self.sessions.add_current_session_changed(
+                    self._current_session_changed_handler
+                )
+            )
+
+            if self.current_session is not None:
+                if self.properties_changed_handler_token is not None:
+                    self.current_session.remove_media_properties_changed(
+                        self.properties_changed_handler_token
+                    )
+                if self.playback_info_changed_handler_token is not None:
+                    self.current_session.remove_playback_info_changed(
+                        self.playback_info_changed_handler_token
+                    )
+
+            self.current_session = self.sessions.get_current_session()
+            if self.current_session:
+                self.properties_changed_handler_token = (
+                    self.current_session.add_media_properties_changed(
+                        self._properties_changed_handler
+                    )
+                )
+                self.playback_info_changed_handler_token = (
+                    self.current_session.add_playback_info_changed(
+                        self._playback_info_changed_handler
+                    )
+                )
+                media_info = MediaInfo()
+                if info := self.current_session.get_playback_info():
+                    media_info.status = info.playback_status.name
+                    media_info.playback_rate = info.playback_rate
+                    media_info.shuffle = info.is_shuffle_active
+                    if info.auto_repeat_mode:
+                        media_info.repeat = info.auto_repeat_mode.name
+                    if info.playback_type:
+                        media_info.type = info.playback_type.name
+                    if info.controls:
+                        media_info.is_fast_forward_enabled = (
+                            info.controls.is_fast_forward_enabled
+                        )
+                        media_info.is_next_enabled = info.controls.is_next_enabled
+                        media_info.is_pause_enabled = info.controls.is_pause_enabled
+                        media_info.is_play_enabled = info.controls.is_play_enabled
+                        media_info.is_previous_enabled = info.controls.is_previous_enabled
+                        media_info.is_rewind_enabled = info.controls.is_rewind_enabled
+                        media_info.is_stop_enabled = info.controls.is_stop_enabled
+
+                if timeline := self.current_session.get_timeline_properties():
+                    media_info.duration = timeline.end_time.total_seconds()
+                    media_info.position = timeline.position.total_seconds()
+
+                if (
+                    properties
+                    := await self.current_session.try_get_media_properties_async()
+                ):
+                    media_info.title = properties.title
+                    media_info.subtitle = properties.subtitle
+                    media_info.artist = properties.artist
+                    media_info.album_artist = properties.album_artist
+                    media_info.album_title = properties.album_title
+                    media_info.track_number = properties.track_number
+
+                media_info.updated_at = datetime.datetime.now().timestamp()
+
+                await self._update_data(media_info)
+
+                if media_info.status == "PLAYING":
+                    self.update_media_info_interval(PLAYING_UPDATE_INTERVAL)
+                else:
+                    self.update_media_info_interval(IDLE_UPDATE_INTERVAL)
+            else:
+                await self._update_data(
+                    MediaInfo(updated_at=datetime.datetime.now().timestamp())
+                )
+        except (OSError, Exception) as error:
+            self._logger.error("Error updating media info: %s", error)
