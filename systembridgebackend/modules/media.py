d880a964907c923532773404235a9690ab822a76
Aidan Timson <aidan@timmo.dev>
2024-08-17 03:53:41 +0100

    Remove check, we can never get here

--- systembridgebackend/modules/media.py
+++ systembridgebackend/modules/media.py
@@ -83,9 +83,6 @@ class Media(Base):
 
     async def update_media_info(self) -> None:
         """Update media info from the current session."""
-        if platform.system() != "Windows":
-            return
-
         try:
             if (
                 self.sessions is not None
@@ -124,7 +121,7 @@ class Media(Base):
                         self._playback_info_changed_handler
                     )
                 )
-                media_info = MediaInfo()
+                media_info = MediaInfo(updated_at=datetime.datetime.now().timestamp())
                 if info := self.current_session.get_playback_info():
                     media_info.status = info.playback_status.name
                     media_info.playback_rate = info.playback_rate
