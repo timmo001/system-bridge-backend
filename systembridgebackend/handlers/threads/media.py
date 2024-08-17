7d83c94474dd0cb7c5abc70dc893d071df95527d
Aidan Timson <aidan@timmo.dev>
2024-08-17 03:38:48 +0100

    Fix media update thread linux updates

--- systembridgebackend/handlers/threads/media.py
+++ systembridgebackend/handlers/threads/media.py
@@ -36,7 +36,7 @@ class MediaUpdateThread(UpdateThread):
     @override
     async def update(self) -> None:
         """Update."""
-        if platform.system() != "Windows" or self.stopping:
+        if self.stopping:
             return
 
         await self._update_cls.update_media_info()
